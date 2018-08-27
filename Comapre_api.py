a25f04692df24eed2283611a4091b8347480ef59
Karthick Aravindan <karthickaravindan@gmail.com>
2018-08-27 18:39:06 +0530

    Add files via upload

--- Comapre_api.py
+++ Comapre_api.py
@@ -9,12 +9,14 @@ import warnings
 import face_recognition.api as face_recognition
 import sys
 import compare_image
+import time
 app = Flask(__name__)
  
 
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
+	t1=time.time()
 	print (request.files)
 	# checking if the file is present or not.
 	# if 'file' not in request.files:
@@ -26,10 +28,11 @@ def upload_file():
 	distance,result = compare_image.main(img_1,img_2)
 	
 	# file.save("static/test.jpg")
-
+	t1=time.time()-t1
 	json_contect={
 					'result':str(result),
-					'distance':distance
+					'distance':distance,
+					'time_taken':str(t1)
 				}
 	python2json = json.dumps(json_contect)
 	return app.response_class(python2json, content_type='application/json') 
